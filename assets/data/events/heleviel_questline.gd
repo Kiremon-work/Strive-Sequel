86593371716636490bb0a3127305e41b91dd9770
ykoc <rak3new@gmail.com>
2026-02-11 03:31:44 +0300

    Update heleviel_questline.gd

--- assets/data/events/heleviel_questline.gd
+++ assets/data/events/heleviel_questline.gd
@@ -452,7 +452,8 @@ var data = {
 				reqs = [
 
 				],
-				dialogue_argument = 1
+				dialogue_argument = 1,
+				change_dialogue_type = 2
 			},
 			{
 				code = "heleviel_quest_13",
@@ -1957,6 +1958,7 @@ var data = {
 
 						],
 						dialogue_argument = 1,
+						change_dialogue_type = 2,
 						type = "next_dialogue"
 					}
 				]
